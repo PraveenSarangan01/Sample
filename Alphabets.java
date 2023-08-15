40640110985564d62d4e583cd0faed7b06e1f06c
PraveenSarangan01 <142241221+PraveenSarangan01@users.noreply.github.com>
2023-08-16 01:10:52 +0530

    Update Alphabets.java
    NAme Added

--- Alphabets.java
+++ Alphabets.java
@@ -9,7 +9,7 @@ public class Alphabets{
 		char[] arrayS2 = str2.toCharArray();
 		Arrays.sort(arrayS1);
 		Arrays.sort(arrayS2);
-		
+		System.out.println("String is not a Praveen")
 		if(Arrays.equals(arrayS1, arrayS2)) {
 			System.out.println("Given String is Anagram");
 		}
